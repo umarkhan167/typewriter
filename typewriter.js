67d22615f0d95a09fce4bf5a4fcf19b1b293bef2
Umar Khan <umarkhan167@outlook.com>
2021-11-15 18:56:52 -0500

    typewriter function now returns a new line at the end

--- typewriter.js
+++ typewriter.js
@@ -1,11 +1,13 @@
 const { time } = require("console");
 
-const sentence = "hello there from lighthouse labs";
+const sentence = "hello there from lighthouse labs\n";
 let delay = 0;
 
 for (const char of sentence) {
+  delay += 50;
   setTimeout(() => {
     process.stdout.write(char);
   }, delay)
-  delay += 50;
-}
+} 
+
+
