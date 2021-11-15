6f4f8e6f4f8ece22430a5c5e5fdb36153f3f8abb
Umar Khan <umarkhan167@outlook.com>
2021-11-15 17:50:51 -0500

    function that creates a typewriter effect on a string

---
+++ typewriter.js
@@ -0,0 +1,11 @@
+const { time } = require("console");
+
+const sentence = "hello there from lighthouse labs";
+let delay = 0;
+
+for (const char of sentence) {
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, delay)
+  delay += 50;
+}
